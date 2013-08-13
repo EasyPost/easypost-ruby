7d095e8d93b0fa69f622e04ab357a3945119de30
Guncha <gunchars@gmail.com>
2013-08-13 01:40:01 -0400

    Implemented #marshal_load and #marshal_dump for EasyPost::EasyPostObject

--- lib/easypost/object.rb
+++ lib/easypost/object.rb
@@ -85,6 +85,15 @@ module EasyPost
       @values[:id]
     end
 
+    def marshal_dump
+      [to_hash, api_key]
+    end
+
+    def marshal_load(ary)
+      initialize(ary.first[:id], ary.last)
+      refresh_from(ary.first, ary.last)
+    end
+
     protected
 
     def metaclass
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -72,4 +72,19 @@ describe EasyPost::Address do
       expect { verified_address = address.verify() }.to raise_error(EasyPost::Error, /Address Not Found./)
     end
   end
+
+  it "can be serialized with Marshal" do
+    address = EasyPost::Address.create(
+      :name => 'Sawyer Bateman',
+      :street1 => '1A Larkspur Cres.',
+      :city => 'St. Albert',
+      :state => 'AB',
+      :zip => 't8n2m4',
+      :country => 'CA'
+    )
+    expect { Marshal.dump(address) }.to_not raise_error(TypeError)
+    serialized_address = Marshal.load(Marshal.dump(address))
+    expect(serialized_address).to be_an_instance_of(EasyPost::Address)
+    expect(serialized_address.zip).to eq('t8n2m4')
+  end
 end
