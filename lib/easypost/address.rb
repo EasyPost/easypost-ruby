ffecbd90b0ff8bae08badf477db750a6aa939f8e
Paul Tyng <paul@paultyng.net>
2014-05-21 22:01:30 -0400

    Add api_key to address methods

--- lib/easypost/address.rb
+++ lib/easypost/address.rb
@@ -1,10 +1,10 @@
 module EasyPost
   class Address < Resource
 
-    def self.create_and_verify(params={})
+    def self.create_and_verify(params={}, api_key=nil)
       wrapped_params = {}
       wrapped_params[self.class_name().to_sym] = params
-      response, api_key = EasyPost.request(:post, url + '/create_and_verify', @api_key, wrapped_params)
+      response, api_key = EasyPost.request(:post, url + '/create_and_verify', api_key, wrapped_params)
 
       if response.has_key?(:address)
         if response.has_key?(:message)
