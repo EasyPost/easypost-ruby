56282eb2b4f478904cadf4b5e40bff5c4f42a9b6
Jake Epstein <jake@easypost.com>
2017-01-19 16:20:30 -0800

    fixed create and added tests for ScanForms

--- lib/easypost/scan_form.rb
+++ lib/easypost/scan_form.rb
@@ -1,4 +1,8 @@
 module EasyPost
   class ScanForm < Resource
+    def self.create(params={}, api_key=nil)
+      response, api_key = EasyPost.request(:post, self.url, api_key, params)
+      return Util.convert_to_easypost_object(response, api_key)
+    end
   end
 end
