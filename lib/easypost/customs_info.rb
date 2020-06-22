a4f6fd51b2d71ae02c371fb83c149fd9eb4dc512
Justintime50 <jhammond@easypost.com>
2020-06-22 15:55:30 -0600

    Made unpaginated methods raise not implemented errors

--- lib/easypost/customs_info.rb
+++ lib/easypost/customs_info.rb
@@ -1,4 +1,9 @@
 module EasyPost
   class CustomsInfo < Resource
+
+    def self.all(filters={}, api_key=nil)
+      raise NotImplementedError.new('CustomsInfo.all not implemented.')
+    end
+
   end
 end
