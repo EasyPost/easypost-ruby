5d9ad66bf3ad455fc57184f54387443e7fd19a4a
Razwan Rashidi <razwan.izmi@gmail.com>
2021-02-09 14:05:45 +0800

    Fix invalid endpoint url in CarrierAccount#types

--- lib/easypost/carrier_account.rb
+++ lib/easypost/carrier_account.rb
@@ -1,6 +1,6 @@
 class EasyPost::CarrierAccount < EasyPost::Resource
   def self.types
-    response = EasyPost.make_request(:get, "/carrier_types", @api_key)
-    return EasyPost::Util.convert_to_easypost_object(response, api_key)
+    response = EasyPost.make_request(:get, "/v2/carrier_types", @api_key)
+    return EasyPost::Util.convert_to_easypost_object(response, @api_key)
   end
 end
