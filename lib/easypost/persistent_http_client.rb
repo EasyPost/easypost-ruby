b62d5f29394f404f33940d62f4df8c8d17398a68
Dmitry Ishkov <dimanrussian@gmail.com>
2021-06-29 10:45:02 +0400

    refactor persistent http client and add comments

--- lib/easypost/persistent_http_client.rb
+++ lib/easypost/persistent_http_client.rb
@@ -5,10 +5,14 @@ class EasyPost::PersistentHttpClient
 
   def self.get(url)
     uri = url.is_a?(URI) ? url : URI(url)
-    connection_manager.get_connection(uri)
+    connection_manager.get_client(uri)
   end
 
+  # each thread gets its own connection manager
   def self.connection_manager
+    # before getting a connection manager
+    # we first clear all old ones
+    remove_old_managers
     Thread.current[:persistent_http_connection_manager] ||= new_manager
   end
 
@@ -17,7 +21,7 @@ class EasyPost::PersistentHttpClient
   end
 
   def self.new_manager
-    remove_old_managers
+    # create a new connection manager in a thread safe way
     mutex.synchronize do
       manager = ConnectionManager.new
       connection_managers << manager
@@ -32,6 +36,8 @@ class EasyPost::PersistentHttpClient
     end
   end
 
+  # mutex isn't needed for CRuby, but might be needed
+  # for other Ruby implementations
   def self.mutex
     @mutex ||= Mutex.new
   end
@@ -75,40 +81,50 @@ class EasyPost::PersistentHttpClient
     end
   end
 
+  # connection manager represents
+  # a cache of all keep-alive connections
+  # in a current thread
   class ConnectionManager
-    attr_accessor :connections, :last_used
+    attr_accessor :clients_store, :last_used
 
     def initialize
-      self.connections = {}
+      self.clients_store = {}
       self.last_used = Time.now
     end
 
-    def get_connection(uri)
+    def get_client(uri)
       unless HttpConfig.persistent_connection?
-        return create_connection(uri)
+        return create_client(uri)
       end
 
       mutex.synchronize do
+        # refresh the last time a client was used,
+        # this prevents the client from becoming stale
         self.last_used = Time.now
 
+        # we use params as a cache key for clients.
+        # 2 connections to the same host but with different
+        # options are going to use different HTTP clients
         params = [uri.host, uri.port]
 
-        connection = connections[params]
+        connection = clients_store[params]
         if connection
           return connection
         end
 
-        connection = create_connection(uri)
+        connection = create_client(uri)
 
+        # open connection
         connection.start
 
-        connections[params] = connection
+        # cache the client
+        clients_store[params] = connection
 
         connection
       end
     end
 
-    def create_connection(uri)
+    def create_client(uri)
       connection = if HttpConfig.config[:proxy]
                      proxy_uri = URI(HttpConfig.config[:proxy])
                      Net::HTTP.new(
@@ -130,6 +146,7 @@ class EasyPost::PersistentHttpClient
 
     def configure_connection(connection)
       connection.use_ssl = true
+      # dynamically set Net::HTTP options
       HttpConfig.config.each do |name, value|
         # Discrepancies between RestClient and Net::HTTP.
         if name == :verify_ssl
@@ -147,10 +164,11 @@ class EasyPost::PersistentHttpClient
       end
     end
 
+    # close connections for each client
     def close_connections!
       mutex.synchronize do
-        connections.values.each(&:finish)
-        self.connections = {}
+        clients_store.values.each(&:finish)
+        self.clients_store = {}
       end
     end
 
