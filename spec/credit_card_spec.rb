b767cc942bba1da3965fccbb7c601aaed31c37fe
jchen293 <jchen@easypost.com>
2022-05-31 15:56:27 -0400

    add Billing functions.

---
+++ spec/credit_card_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe EasyPost::CreditCard, :authenticate_prod do
+  describe '.fund' do
+    xit 'fund a credit card' do
+      # Skipping due to the lack of an available real credit card in tests
+      credit_card = described_class.fund('100', 'primary')
+
+      expect(credit_card).not_to be_nil
+    end
+  end
+
+  describe '.delete' do
+    xit 'delete a credit card by ID' do
+      # Skipping due to the lack of an available real credit card in tests
+      deleted_credit_card = described_class.delete('card_123')
+
+      expect(deleted_credit_card).not_to be_nil
+    end
+  end
+end
