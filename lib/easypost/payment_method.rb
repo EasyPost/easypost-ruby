0c9c0a13b20886861c4a701098c7a9f19c6b2348
jchen293 <jchen@easypost.com>
2022-06-03 12:32:10 -0400

    add missing conditional to check if there is a payment method. (#181)

--- lib/easypost/payment_method.rb
+++ lib/easypost/payment_method.rb
@@ -2,4 +2,14 @@
 
 # PaymentMethod objects represent a payment method of a user.
 class EasyPost::PaymentMethod < EasyPost::Resource
+  # Retrieve all payment methods.
+  def self.all(api_key = nil)
+    response = EasyPost.make_request(:get, url, api_key)
+
+    if response['id'].nil?
+      raise EasyPost::Error.new('Billing has not been setup for this user. Please add a payment method.')
+    end
+
+    EasyPost::Util.convert_to_easypost_object(response, api_key)
+  end
 end
