f4a9b9415c8cd3decd9996c0ade7943db58d7759
jchen293 <jchen@easypost.com>
2022-07-25 11:47:05 -0400

    change the deprecation wording

--- lib/easypost/payment_method.rb
+++ lib/easypost/payment_method.rb
@@ -3,7 +3,7 @@
 # PaymentMethod objects represent a payment method of a user.
 class EasyPost::PaymentMethod < EasyPost::Resource
   # <b>DEPRECATED:</b> Please use <tt>Billing class</tt> instead.
-  # Last working version v4.5.0. Removal v6.0.0.
+  # Deprecated: v4.5.0 - v6.0.0
   def self.all(api_key = nil)
     warn '[DEPRECATION] `all` is deprecated.  Please use `Billing.retrieve_payment_methods` instead.'
     EasyPost::Billing.retrieve_payment_methods(api_key)
