f1c308f9901590f6829d3647ca68ca6fa702dd61
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-08-01 13:51:52 -0600

    fix: move carbon offset tests/cassettes to correct location

--- spec/shipment_spec.rb
+++ spec/shipment_spec.rb
@@ -62,6 +62,29 @@ describe EasyPost::Shipment do
       expect(shipment.parcel.id).to match('prcl_')
       expect(shipment.from_address.street1).to eq('388 Townsend St')
     end
+
+    it 'creates a shipment with carbon_offset' do
+      shipment = described_class.create(Fixture.basic_carbon_offset_shipment, nil, true)
+
+      expect(shipment).to be_an_instance_of(described_class)
+      expect(shipment.rates).not_to be_nil
+
+      rate = shipment.rates.first
+      expect(rate.carbon_offset).not_to be_nil
+    end
+
+    it 'creates a one-call-buy shipment with carbon_offset' do
+      shipment = described_class.create(Fixture.one_call_buy_carbon_offset_shipment, nil, true)
+
+      expect(shipment.fees).not_to be_nil
+      carbon_offset_found = false
+      shipment.fees.each do |fee|
+        if fee.type == 'CarbonOffsetFee'
+          carbon_offset_found = true
+        end
+      end
+      expect(carbon_offset_found).to be_truthy
+    end
   end
 
   describe '.retrieve' do
@@ -98,6 +121,23 @@ describe EasyPost::Shipment do
 
       expect(shipment.postage_label).not_to be_nil
     end
+
+    it 'buys a shipment with carbon offset' do
+      shipment = described_class.create(Fixture.full_carbon_offset_shipment, nil)
+
+      shipment.buy(shipment.lowest_rate, true)
+
+      expect(shipment.postage_label).not_to be_nil
+
+      expect(shipment.fees).not_to be_nil
+      carbon_offset_found = false
+      shipment.fees.each do |fee|
+        if fee.type == 'CarbonOffsetFee'
+          carbon_offset_found = true
+        end
+      end
+      expect(carbon_offset_found).to be_truthy
+    end
   end
 
   describe '.regenerate_rates' do
@@ -110,6 +150,16 @@ describe EasyPost::Shipment do
 
       expect(rates_array).to be_an_instance_of(Array)
       expect(rates_array).to all(be_an_instance_of(EasyPost::Rate))
+    end
+
+    it 'regenerates rates for a shipment with carbon offset' do
+      shipment = described_class.create(Fixture.full_carbon_offset_shipment)
+
+      shipment_with_carbon_offset = shipment.regenerate_rates(true)
+
+      shipment_with_carbon_offset.rates.each do |rate|
+        expect(rate.carbon_offset).not_to be_nil
+      end
     end
   end
 
