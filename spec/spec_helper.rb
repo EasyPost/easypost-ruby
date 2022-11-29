ec55074ac92722cea1edb5dad48e8a64ff82e3dc
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-11-29 13:12:49 -0700

    feat: expire cassettes after 180 days

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,14 +33,35 @@ RSpec.configure do |config|
     # Automatically wrap the test in VCR to avoid forgetting it.
     path = example.file_path.gsub('_spec.rb', '').gsub('./spec/', '')
     description = example.full_description
+    cassette_path = "#{path}/#{description}"
     VCR.use_cassette(
-      "#{path}/#{description}",
+      cassette_path,
       allow_unused_http_interactions: true,
     ) do
       example.call
     end
 
+    check_expired_cassette(cassette_path)
+
     # Reset `EasyPost.http_config` to avoid flakey tests.
     EasyPost.reset_http_config
   end
+end
+
+# Checks for an expired cassette and warns if it is too old and must be re-recorded
+def check_expired_cassette(cassette_path)
+  full_cassette_path = "#{"spec/cassettes/#{cassette_path}".gsub('::', '_').gsub(' ', '_').gsub('.', '_')}.yml"
+  seconds_in_day = 86_400
+  expiration_days = 180
+  expiration_seconds = (seconds_in_day * expiration_days)
+
+  return unless File.exist?(full_cassette_path)
+
+  cassette_timestamp = File.mtime(full_cassette_path)
+  expiration_timestamp = DateTime.parse((cassette_timestamp + expiration_seconds).to_s)
+  current_timestamp = DateTime.now
+
+  return unless current_timestamp > expiration_timestamp
+
+  warn "#{full_cassette_path} is older than #{expiration_days} days and has expired. Please re-record the cassette"
 end
