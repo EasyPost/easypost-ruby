af23fb0d0e4d2e11c8f536f76d9e57de1c1cf1f8
nharris <nharris@easypost.com>
2023-01-09 15:18:09 -0700

    - Linting

--- lib/easypost/beta/payment_refund.rb
+++ lib/easypost/beta/payment_refund.rb
@@ -2,5 +2,4 @@
 
 # PaymentRefund objects represent a refund of a payment.
 class EasyPost::Beta::PaymentRefund < EasyPost::Resource
-
 end
--- lib/easypost/payment_method.rb
+++ lib/easypost/payment_method.rb
@@ -2,7 +2,6 @@
 
 # PaymentMethod objects represent a payment method of a user.
 class EasyPost::PaymentMethod < EasyPost::Resource
-
   # List all payment methods associated with the current user.
   # <b>DEPRECATED:</b> Please use <tt>Billing.retrieve_payment_methods</tt> instead.
   # Deprecated: v4.5.0 - v6.0.0
