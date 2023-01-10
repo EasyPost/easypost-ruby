c4e0b0a6a4674b3ba55a660af2e209d54ed2855d
Nate Harris <nwithan8@users.noreply.github.com>
2023-01-10 15:45:04 -0700

    Fix failing deserialization (#236)
    - Fix bug not pulling key-value pair from hash during JSON deserialization
    - Add unit tests to confirm deserialization logic branches

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -45,4 +45,87 @@ describe EasyPost::Util do
         .to eq({ 'parent_key[nested_key]' => '123' })
     end
   end
+
+  describe '.convert_to_easypost_object' do
+    it 'converts a hash to a specific class by matching ID prefix' do
+      data = {
+        id: 'adr_123',
+      }.to_hash
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      expect(object).to be_a(EasyPost::Address)
+    end
+
+    it 'converts a hash to a specific class by matching object type' do
+      data = {
+        object: 'Address',
+      }
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      expect(object).to be_a(EasyPost::Address)
+    end
+
+    it 'converts a hash to a generic EasyPostObject if no matching ID or object type' do
+      data = {
+        id: 'foo_123',
+        object: 'Nothing',
+      }
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      expect(object).to be_a(EasyPost::EasyPostObject)
+    end
+
+    it 'converts a hash to a generic EasyPostObject if missing ID and object type' do
+      data = {
+        random_key: 'random_value',
+      }
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      expect(object).to be_a(EasyPost::EasyPostObject)
+    end
+
+    it 'converts sub-hashes to EasyPost object' do
+      data = {
+        'id' => '123',
+        'primary_payment_method' => {
+          'id' => 'bank_123',
+        },
+        'secondary_payment_method' => {
+          'id' => 'card_123',
+        },
+      }
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      # No matching ID prefix or "object" key means the outer object will just be deserialized to an EasyPostObject
+      expect(object).to be_a(EasyPost::EasyPostObject)
+
+      # The sub-hashes have proper prefixes, so they will be converted to their respective objects
+      expect(object[:primary_payment_method]).to be_an_instance_of(EasyPost::PaymentMethod)
+      expect(object[:secondary_payment_method]).to be_an_instance_of(EasyPost::PaymentMethod)
+    end
+
+    it 'converts an array of hashes to an array of EasyPostObjects' do
+      data = [
+        {
+          'id' => 'foo_123',
+        },
+      ]
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      expect(object).to be_an(Array)
+      expect(object.first).to be_a(EasyPost::EasyPostObject)
+    end
+
+    it 'converts an array of hashes to an array of specific classes if matching ID prefix or object type' do
+      data = [
+        {
+          'id' => 'adr_123',
+        },
+      ]
+      object = described_class.convert_to_easypost_object(data, nil)
+
+      expect(object).to be_an(Array)
+      expect(object.first).to be_a(EasyPost::Address)
+    end
+  end
 end
