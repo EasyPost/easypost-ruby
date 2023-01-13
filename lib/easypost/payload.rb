92cf5b7dd41ff409c97adaa546acb78507c9b652
nharris <nharris@easypost.com>
2023-01-13 14:22:14 -0700

    - Address feedback, part 2

--- lib/easypost/payload.rb
+++ lib/easypost/payload.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'json'
-
 # Webhook Event Payloads are triggered by changes in objects you've created via the API.
 class EasyPost::Payload < EasyPost::Resource
 end
