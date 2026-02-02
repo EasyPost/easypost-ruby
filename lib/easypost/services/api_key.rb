2df9984b6966dbdc882f5952e74f10528ce06300
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2026-02-02 14:17:24 -0700

    feat: create/delete/enable/disable API keys

--- lib/easypost/services/api_key.rb
+++ lib/easypost/services/api_key.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class EasyPost::Services::ApiKey < EasyPost::Services::Service
+  MODEL_CLASS = EasyPost::Models::ApiKey # :nodoc:
+
   # Retrieve a list of all ApiKey objects.
   def all
     response = @client.make_request(:get, 'api_keys')
 
-    EasyPost::InternalUtilities::Json.convert_json_to_object(response, EasyPost::Models::ApiKey)
+    EasyPost::InternalUtilities::Json.convert_json_to_object(response, MODEL_CLASS)
   end
 
   # Retrieve a list of ApiKey objects (works for the authenticated user or a child user).
@@ -25,5 +27,34 @@ class EasyPost::Services::ApiKey < EasyPost::Services::Service
     end
 
     raise EasyPost::Errors::FilteringError.new(EasyPost::Constants::NO_USER_FOUND)
+  end
+
+  # Create an API key for a child or referral customer user
+  def create(mode)
+    response = @client.make_request(:post, 'api_keys', { mode: mode })
+
+    EasyPost::InternalUtilities::Json.convert_json_to_object(response, MODEL_CLASS)
+  end
+
+  # Delete an API key for a child or referral customer user
+  def delete(id)
+    @client.make_request(:delete, "api_keys/#{id}")
+
+    # Return true if succeeds, an error will be thrown if it fails
+    true
+  end
+
+  # Enable an API key for a child or referral customer user
+  def enable(id)
+    response = @client.make_request(:post, "api_keys/#{id}/enable")
+
+    EasyPost::InternalUtilities::Json.convert_json_to_object(response, MODEL_CLASS)
+  end
+
+  # Disable an API key for a child or referral customer user
+  def disable(id)
+    response = @client.make_request(:post, "api_keys/#{id}/disable")
+
+    EasyPost::InternalUtilities::Json.convert_json_to_object(response, MODEL_CLASS)
   end
 end
